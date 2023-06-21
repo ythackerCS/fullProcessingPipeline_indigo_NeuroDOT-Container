453286e7830c0b07706c2a9dfa1ae50fabe43cd7
Yash Thacker <ythacker@wustl.edu>
2023-06-21 18:20:05 -0500

    added sklearn

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 cp Dockerfile.base Dockerfile && \
 ./command2label.py ./xnat/command.json >> Dockerfile && \
-docker build --no-cache -t xnat/fullprocessingneurodot_bids_v2:3.1 .
-docker tag xnat/fullprocessingneurodot_bids_v2:3.1 registry.nrg.wustl.edu/docker/nrg-repo/yash/fullprocessingneurodot_bids_v2:3.1
+docker build --no-cache -t xnat/fullprocessingneurodot_bids_v2:3.2 .
+docker tag xnat/fullprocessingneurodot_bids_v2:3.2 registry.nrg.wustl.edu/docker/nrg-repo/yash/fullprocessingneurodot_bids_v2:3.2
 rm Dockerfile
