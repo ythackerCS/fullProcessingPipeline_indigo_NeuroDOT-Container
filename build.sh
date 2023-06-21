44b640d5f1c53b8c1bb8c6ade398d3bfdaa5d568
Yash Thacker <ythacker@wustl.edu>
2023-06-21 16:28:11 -0500

    updated versions in docker files

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 cp Dockerfile.base Dockerfile && \
 ./command2label.py ./xnat/command.json >> Dockerfile && \
-docker build --no-cache -t xnat/fullprocessingneurodot_bids:latest .
-docker tag xnat/fullprocessingneurodot_bids:latest registry.nrg.wustl.edu/docker/nrg-repo/yash/fullprocessingneurodot_bids:latest
+docker build --no-cache -t xnat/fullprocessingneurodot_bids_v2:2.0 .
+docker tag xnat/fullprocessingneurodot_bids_v2:2.0 registry.nrg.wustl.edu/docker/nrg-repo/yash/fullprocessingneurodot_bids_v2:2.0
 rm Dockerfile
