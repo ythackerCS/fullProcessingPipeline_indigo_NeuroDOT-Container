1200aa1e6988ae1e7e25324438758feb89a60029
ythackerCS <46691555+ythackerCS@users.noreply.github.com>
2023-06-21 18:13:52 -0500

    updated versions to 3.1

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 cp Dockerfile.base Dockerfile && \
 ./command2label.py ./xnat/command.json >> Dockerfile && \
-docker build --no-cache -t xnat/fullprocessingneurodot_bids_v2:3.0 .
-docker tag xnat/fullprocessingneurodot_bids_v2:3.0 registry.nrg.wustl.edu/docker/nrg-repo/yash/fullprocessingneurodot_bids_v2:3.0
+docker build --no-cache -t xnat/fullprocessingneurodot_bids_v2:3.1 .
+docker tag xnat/fullprocessingneurodot_bids_v2:3.1 registry.nrg.wustl.edu/docker/nrg-repo/yash/fullprocessingneurodot_bids_v2:3.1
 rm Dockerfile
